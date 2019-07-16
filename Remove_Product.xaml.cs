8e09fdc8efa12f48b68367ddc643c78a5c70d641
= <=>
2019-07-16 11:19:10 -0500

    Pages created, Add Employee branch created

---
+++ Remove_Product.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace La_Bakéry
+{
+    /// <summary>
+    /// Interaction logic for Remove_Product.xaml
+    /// </summary>
+    public partial class Remove_Product : Window
+    {
+        public Remove_Product()
+        {
+            InitializeComponent();
+        }
+    }
+}
