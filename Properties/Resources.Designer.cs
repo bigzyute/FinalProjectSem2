f51afa47826f9c140241f98ab1f71e3fe2ee05c7
bigzyute <orainemc@gmail.com>
2019-07-21 17:10:51 -0500

    Dashboard: current user 1.2

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace La_Bakéry.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap default_user {
+            get {
+                object obj = ResourceManager.GetObject("default_user", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon login_icon {
