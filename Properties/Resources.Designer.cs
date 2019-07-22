5bed5480f3a035ff5bd05bebcec41f10f9f3b8a9
bigzyute <orainemc@gmail.com>
2019-07-21 21:45:17 -0500

    Dashboard: navigation panel 1.1

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -109,5 +109,25 @@ namespace La_Bakéry.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap product_icon {
+            get {
+                object obj = ResourceManager.GetObject("product_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap report_icon {
+            get {
+                object obj = ResourceManager.GetObject("report_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
